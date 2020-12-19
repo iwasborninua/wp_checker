038222da1d9005940150aecda024eaee0f07a745
Michael Kharchenko <mihail.macnamara@gmail.com>
2020-12-19 23:09:18 +0200

    some fix

--- async_check.php
+++ async_check.php
@@ -16,6 +16,7 @@ use function Amp\Sync\ConcurrentIterator\each;
 
 $verified = fopen('data/verified.txt', 'w+');
 $bad = fopen('data/bad.txt', 'w+');
+$bad_form = fopen('data/form_not_found.txt', 'w+');
 
 // Устанавливаем конфиг гугловского DNS
 Dns\resolver(new Dns\Rfc1035StubResolver(null, new class implements Dns\ConfigLoader {
@@ -32,7 +33,7 @@ Dns\resolver(new Dns\Rfc1035StubResolver(null, new class implements Dns\ConfigLo
     }
 }));
 
-Loop::run(function () use ($verified, $bad) {
+Loop::run(function () use ($verified, $bad, $bad_form) {
     $iterator = new Producer(function ($emit) {
         $file = fopen('data/domains.txt', 'r');
         while (false !== $line = fgets($file)) {
@@ -45,9 +46,11 @@ Loop::run(function () use ($verified, $bad) {
         ->followRedirects(0)
         ->build();
 
-    yield each($iterator, new LocalSemaphore(30), function ($line) use ($client, $client_disable_redirect, $verified, $bad) {
+    yield each($iterator, new LocalSemaphore(30), function ($line) use (
+        $client, $client_disable_redirect, $verified, $bad, $bad_form
+    ) {
         $request = new Request( DP . $line . WP_ADMIN_PATH);
-        $request->setTcpConnectTimeout(2400);
+        $request->setTcpConnectTimeout(4000);
         try {
             $response = yield $client->request($request);
             if ($response->getStatus() == 200) {
@@ -55,7 +58,7 @@ Loop::run(function () use ($verified, $bad) {
                     ->filter('form#loginform')->count();
                 if ($login_form > 0) {
                     $author_request = new Request(DP . $line . AUTHOR_DORK);
-                    $author_request->setTcpConnectTimeout(2400);
+                    $author_request->setTcpConnectTimeout(4000);
                     $response = yield $client_disable_redirect->request($author_request);
 
                     if ($response->getStatus() == "301") {
@@ -63,7 +66,7 @@ Loop::run(function () use ($verified, $bad) {
                         $location_array = array_diff(explode('/', $location), LOCATION_URL_RULES);
                         $login = end($location_array);
 
-                        if (count($location_array) == 2 && $line != end($location_array)) {
+                        if (count($location_array) >= 2 && $line != end($location_array)) {
                             echo "verifed|login: {$line}" . PHP_EOL;
                             $url = DP . $line . "/wp-login.php;" . $login;
                         } else {
@@ -83,7 +86,7 @@ Loop::run(function () use ($verified, $bad) {
                     fwrite($bad,$line. PHP_EOL);
                 }
             } else {
-                fwrite($bad, $line . PHP_EOL);
+                fwrite($bad_form, DP . $line . WP_ADMIN_PATH . PHP_EOL);
             }
         } catch (Exception $e) {
             echo "Bad request: " . $line . PHP_EOL;
